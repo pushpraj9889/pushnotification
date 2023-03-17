f092f7432169da51321ccb72a1b8bdea010449e8
pushpraj9889 <pushprajsingh5821@gmail.com>
2023-03-17 12:44:42 +0530

    comments in code

--- App.tsx
+++ App.tsx
@@ -1,55 +1,13 @@
-import React, {useEffect} from 'react';
-import {View, Text, Button, StyleSheet, Alert} from 'react-native';
-import messaging from '@react-native-firebase/messaging';
-// utils import
-import {
-  ForeGroundHandler,
-  intialNotification,
-  notificationListner,
-} from './src/utils/foregGroundHandler';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Pushnotification from './src/notificationScreen';
 
-const Pushnotification = () => {
-  useEffect(() => {
-    notificationListner((rep: any) => {
-      console.log(rep);
-    });
-    intialNotification((resp: any) => {
-      console.log(resp);
-    });
-
-    //  when app will be in foreground state
-  }, []);
-  {
-    ForeGroundHandler();
-  }
-
-  const checkToken = async () => {
-    const fcmToken = await messaging().getToken();
-    if (fcmToken) {
-      console.log(fcmToken);
-      Alert.alert(fcmToken);
-    }
-  };
+export default function App() {
   return (
-    <View style={styles.Container}>
-      <Text style={styles.paragraph}>
-        {'Push Notification With Firebasse Demo'}
-      </Text>
-      <Button title="Get FCM Token" onPress={checkToken} />
+    <View>
+      <Pushnotification />
     </View>
   );
-};
-const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  paragraph: {
-    textAlign: 'center',
-    fontSize: 22,
-    color: 'black',
-    fontWeight: 'bold',
-  },
-});
-export default Pushnotification;
+}
+
+const styles = StyleSheet.create({});
